f2ade1bb15fb7c846849225b49f8bf4a0f73fce0
Supachai Ruknuy <supachai5028@gmail.com>
2023-09-28 21:21:05 -0400

    commit

---
+++ modules/legoSets.js
@@ -0,0 +1,147 @@
+/********************************************************************************* * 
+ * WEB322 – Assignment 2
+ * * I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+ * * No part of this assignment has been copied manually or electronically from any other source 
+ * * (including web sites) or distributed to other students. * 
+ * * Name: Supachai Ruknuy 
+ *   Student ID: 121707228 
+ *   Date: 28-Sep-2023
+ *  
+ * * ********************************************************************************/
+
+// Read both files
+const setData = require("../data/setData");
+const themeData = require("../data/themeData");
+
+// Create an empty array
+let sets = [];
+
+/////////////////// Required Functions //////////////////////
+
+//This function is to fill the "sets" array
+function Initialize()
+{
+    return new Promise((resolve, reject)=> {
+
+        // Loop through each element in the setData object
+    setData.forEach(set => 
+        {
+            // Find theme data based on theme_id
+            const theme = themeData.find(theme => theme.id === set.theme_id);
+            
+            // Validate result, adding to the theme attribute
+            if(theme)
+            {
+                set.theme = theme.name;
+            }
+    
+            // Push the new element to the object
+            sets.push(set);
+    
+        });
+
+        // Resolve the promise with no data
+        resolve();
+
+    });    
+
+}
+
+
+// This function simply returns the complete "sets" array
+function getAllsets()
+{ 
+    return new Promise((resolve, reject)=>{
+
+        if(sets.length ===0)
+        {
+            reject("The sets array is empty. Please call initialize the array")
+        }
+        else
+        {
+             // Resolve the promise the sets
+             resolve(sets);
+        }
+
+    })
+}
+
+// This function will return a specific "set" that matched with the setNum
+function getSetByNum(setNum)
+{
+    return new Promise((resolve, reject) => {
+
+        // Find the set_num
+        const foundSetNum = sets.find(set => set.set_num == setNum);
+
+        if(foundSetNum)
+        {
+            // Resolve with the mathcing result
+            resolve(foundSetNum);
+
+        }else{
+            // Reject the result
+            reject(`Error: Not found the set with set_num: ${setNum}`);
+
+    }
+
+    });
+}
+
+
+// This function will return a specific "set" that matched with the theme
+function getSetsByTheme(theme)
+{
+    return new Promise((resolve, reject) => {
+
+         // Convert to lower case
+        const themeLowerCase = theme.toLowerCase();
+
+        // Use the filter method to find sets matching the theme
+        const matchedResult = sets.filter(set => 
+        {
+            // Convert the set to lower case 
+            const setLowerCase = set.theme.toLowerCase();
+
+            // Check the letter
+            return setLowerCase.includes(themeLowerCase);
+
+    });
+
+        if(matchedResult.length>0)
+        {
+            // Resolve with the mathcing result
+            resolve(matchedResult);
+        }else{
+
+            // Reject the result
+            reject(`Error: Not found the set with theme: ${theme}`);
+
+        }
+  
+    });
+   
+}
+
+// Export functions as a module
+module.exports = {Initialize, getAllsets, getSetByNum, getSetsByTheme};
+
+
+// Test functions [result: OK]
+ /* 
+//Load all sets
+Initialize();
+
+//Get all set
+const allset= getAllsets();
+console.log(allset);
+
+// get set_num
+const selectNum = getSetByNum("6539-1");
+console.log(selectNum);
+
+// get theme
+const selectTheme = getSetsByTheme("Technic");
+console.log(selectTheme);
+*/
+
---
+++ server.js
@@ -0,0 +1,73 @@
+/********************************************************************************
+* WEB322 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Supachai Ruknuy Student ID: 121707228 Date:28-Sep-2023
+*
+********************************************************************************/
+
+const legoData = require("./modules/legoSets");
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// This route simply sends back the text
+app.get('/',(reg, res)=>{
+    res.send('Assignment 2: Supachai Ruknuy - 121707228');
+});
+
+// This route is responsible for responding with all of the Lego sets
+app.get('/lego/sets',(reg,res) => {
+
+    //get allsets 
+    legoData.getAllsets().then(sets=>{
+        res.json(sets);
+    }).catch(error=>{
+        res.status(500).send(error); // catch teh errors
+    });
+
+});
+
+
+// Invoking the function with a known setNum value from your data set.
+app.get('/lego/sets/num-demo',(reg,res)=>{
+
+    const setNum = '001-1'; // input value
+
+    legoData.getSetByNum(setNum).then(sets=> {
+
+        res.json(sets);
+
+    }).catch(error=>{
+
+        res.status(500).send(error); // catch the errors
+    });
+
+});
+
+
+// Invoking the function with a known theme value from your data set
+app.get('/lego/sets/theme-demo',(reg,res)=>{
+
+    const theme = 'tech'; // replace with a known theme value
+
+    legoData.getSetsByTheme(theme).then(sets=> {
+        res.json(sets);
+    }).catch(error=> {
+        res.status(500).send(error); // catch the errors
+    });
+
+});
+
+// Deliver to the Express server
+app.listen(port,() => {
+
+    console.log(`The server is running port ${port}`);
+
+});
